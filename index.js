b7e6fff35715220b09b536d31eba24907414fb20
VolodymyrKorol <volodymyr01korol@gmail.com>
2020-10-10 23:01:34 +0300

    Init]

---
+++ index.js
@@ -0,0 +1,4 @@
+$('#about-slider').slick({
+    nextArrow: '<div class="arrow-slider arrow-slider-right"><img src="img/right-arrow.png" alt=">"></div>',
+    prevArrow: '<div class="arrow-slider arrow-slider-left"><img src="img/left-arrow.png" alt="<"></div>'
+});
